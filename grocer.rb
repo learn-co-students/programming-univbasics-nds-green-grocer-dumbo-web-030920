136d8f4afb1dca287af9d7d7f933b7cc499213fe
Xavier Carty <xaviercarty@gmail.com>
2019-12-31 01:00:29 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -15,15 +15,40 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    coupon_name = coupon[:item]
+    coupon_item_num = coupon[:num]
+    cart_item = cart[coupon_name]
+
+    next if cart_item.nil? || cart_item[:count] < coupon_item_num
+
+    cart_item[:count] -= coupon_item_num
+
+    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
+    if coupon_in_cart
+      coupon_in_cart[:count] += 1
+    else
+      cart["#{coupon_name} W/COUPON"] = { 
+        price: coupon[:cost], 
+        clearance: cart_item[:clearance], 
+        count: 1
+      }
+    end
 end
 
-def apply_clearance(cart)
+def apply_clearance(cart:[])
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |item_name, item_data|
+    if item_data[:clearance]
+      item_data[:price] = (item_data[:price] * 0.8).round(1)
+    end
+  end
 end
 
-def checkout(cart, coupons)
+def checkout(cart[], coupons[])
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -33,4 +58,18 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  cart = consolidate_cart(cart: cart)
+	
+  cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  total = 0
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
+  end
+
+  if total > 100
+    0.9 * total
+  else
+    total
+  end
 end
